4690ab1d6d3b4476e1c90f6fb1e2023875fbeaab
mydevby <mydev.by@gmail.com>
2025-02-06 11:34:43 +0300

    Update video block (home page)

--- wp-content/themes/site/front-page.php
+++ wp-content/themes/site/front-page.php
@@ -70,20 +70,21 @@
         </div>
     </div>
 </div>
-<?get_template_part( 'templates/parts/expert', null,array('class'=>'expert-reverse') );?>
+
+<?php get_template_part( 'templates/parts/expert', null, array('class'=>'expert-reverse') ); ?>
+
 <div class="video-block">
-    <div class="container">
-        <div class="top-text text-center">YouTube- канал</div>
-        <div class="name text-center">Экскурс в мир текстиля</div>
-        <div class="videos">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
+	<div class="container">
+		<div class="top-text text-center">YouTube- канал</div>
+		<div class="name text-center">Экскурс в мир текстиля</div>
+		<div class="videos">
+			<div class="swiper-container">
+				<div class="swiper-wrapper">
+					
 					<?php if (have_rows('video_info')) {
-						// Цикл по повторяющемуся полю "contacts"
+						
 						while (have_rows('video_info')) {
 							the_row();
-							
-							// Получаем значения вложенных полей
 							$video_name = get_sub_field('video_name');
 							$video_img = get_sub_field('video_img');
 							$video_link = get_sub_field('video_link');
@@ -96,28 +97,27 @@
 									</div>
 									<p><?php echo $video_name; ?></p>
 								</a>
-							
-							
 							</div>
-							
-							<?php
-						}
-					}
-					?>
-
-                </div>
-
-                <div class="swiper-button-next"> <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                        <path d="M12 24L20 16L12 8" stroke="#26231F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg></div>
-                <div class="swiper-button-prev"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                        <path d="M20 24L12 16L20 8" stroke="#26231F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg></div>
-            </div>
-
-        </div>
-    </div>
+						
+						<?php } } ?>
+				
+				</div>
+				
+				<div class="swiper-button-next">
+					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+						<path d="M12 24L20 16L12 8" stroke="#26231F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+				</div>
+				<div class="swiper-button-prev">
+					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+						<path d="M20 24L12 16L20 8" stroke="#26231F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
+
 <div class="news-block">
     <div class="container">
         <div class="top-text">Блог</div>
